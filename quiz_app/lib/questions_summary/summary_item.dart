dd956bda7c6942175164b4578364f6b1b0924457
Canbozyigitt <136346490+Canbozyigitt@users.noreply.github.com>
2024-05-05 17:29:16 +0300

    Update summary_item.dart

--- quiz_app/lib/questions_summary/summary_item.dart
+++ quiz_app/lib/questions_summary/summary_item.dart
@@ -15,7 +15,7 @@ class SummaryItem extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.symmetric(
-        vertical: 9,
+        vertical: 8,
       ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
